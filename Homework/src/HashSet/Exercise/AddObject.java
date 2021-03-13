9ef9e90b1db0141aae7b322f4ed0af868bc94352
Deepak <deepak_rai10@yahoo.com>
2021-03-13 22:52:51 +0545

    updated

---
+++ Homework/src/HashSet/Exercise/AddObject.java
@@ -0,0 +1,15 @@
+package HashSet.Exercise;
+
+import java.util.HashSet;
+
+public class AddObject {
+    public void addObject(){
+        HashSet<String> hashList=new HashSet<String>();
+        hashList.add("First Item");
+        hashList.add("Second Item");
+        hashList.add("Third Item");
+        hashList.add("Fourth Item");
+        hashList.add("Fifth Item");
+        System.out.println(hashList);
+    }
+}
---
+++ Homework/src/HashSet/Exercise/Output.java
@@ -0,0 +1,8 @@
+package HashSet.Exercise;
+
+public class Output {
+    public static void main(String[] args) {
+        AddObject obj=new AddObject();
+        obj.addObject();
+    }
+}
